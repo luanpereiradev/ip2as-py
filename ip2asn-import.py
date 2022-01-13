5f49932fc8d69ff238194c8cdd49db8b9782160e
Luan Pereira <luanpereiradev1@gmail.com>
2022-01-13 15:51:52 -0300

    Melhorias

--- ip2asn-import.py
+++ ip2asn-import.py
@@ -3,22 +3,26 @@ import ipaddress
 import json
 import redis
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+redisClient = redis.Redis(host='localhost', port=6379, db=0)
+redisClient.delete('ip_table')
 
-with open("ip2asn-v4.tsv") as fd:
-    rd = csv.reader(fd, delimiter="\t", quotechar='"')
+with open("dbip-asn-lite-2022-01.csv") as fd:
+    rd = csv.reader(fd)
     for row in rd:
-        mapped = {
-            'start_ip': row[0],
-            'end_ip': row[1],
-            'as': {
-                'asn': int(row[2]),
-                'name': row[4],
-                'country': row[3],
-            },
-        }
-        score = int(ipaddress.ip_address(row[1]))
-        r.zadd('tabela_ip', {json.dumps(mapped): score})
-        print(mapped)
+        as_name = row[3].lstrip("\"").rstrip("\"")
+        as_number = int(row[2])
 
-r.close()
+        if ipaddress.ip_address(row[0]).version == 4:
+            mapped = {
+                'start_ip': row[0],
+                'end_ip': row[1],
+                'as': {
+                    'asn': as_number,
+                    'name': as_name,
+                },
+            }
+            score = int(ipaddress.ip_address(row[1]))
+            redisClient.zadd('ip_table', {json.dumps(mapped): score})
+            print(row)
+
+redisClient.close()
--- ip2asn-query.py
+++ ip2asn-query.py
@@ -3,19 +3,53 @@ import redis
 import json
 import datetime
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+
+def isBogon(ip):
+    ip_address = ipaddress.ip_address(ip)
+
+    networks = [
+        "0.0.0.0/8",
+        "10.0.0.0/8",
+        "100.64.0.0/10",
+        "127.0.0.0/8",
+        "169.254.0.0/16",
+        "172.16.0.0/12",
+        "192.0.0.0/24",
+        "192.0.2.0/24",
+        "192.88.99.0/24",
+        "192.168.0.0/16",
+        "198.18.0.0/15",
+        "198.51.100.0/24",
+        "203.0.113.0/24",
+        "240.0.0.0/4",
+        "255.255.255.255/32",
+        "224.0.0.0/4",
+    ]
+
+    for network in networks:
+        if ip_address in ipaddress.ip_network(network):
+            return True
+
+    return False
+
+
+redisClient = redis.Redis(host='localhost', port=6379, db=0)
 
 ip = input('Digite o endereço IP: ')
 
-a = datetime.datetime.now()
+if isBogon(ip):
+    print('Bogon IP Address!')
+    exit(1)
 
-value = r.zrangebyscore('ip_table', int(
-    ipaddress.ip_address(ip)), 'inf', 0, 1)
+startTime = datetime.datetime.now()
 
-b = datetime.datetime.now()
-delta = b-a
+value = redisClient.zrangebyscore('ip_table', int(
+    ipaddress.ip_address(ip)), '+inf', 0, 1)
+
+endTime = datetime.datetime.now()
+deltaTime = endTime-startTime
 
 print(json.loads(value[0].decode('utf8')))
-print('Tempo para responder: {}ms'.format(delta.total_seconds() * 1000))
+print('Tempo para responder: {}ms'.format(deltaTime.total_seconds() * 1000))
 
-r.close()
+redisClient.close()
